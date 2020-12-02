28b95ad1d0b14389fee2d4b109f210efb469d0b9
Ronjini Hassan <ronjinihassan@ronjinis-mbp.home>
2020-12-02 12:45:26 -0500

    Reading the csv file in PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,6 +5,18 @@ import csv
 #Set path for file
 budget_path = os.path.join("Resources", "budget_data.csv")
 
+#Set variables 
+total_months = 0
+total_profitlosses = 0
 
+#Open budget_data as a csv file
+with open(budget_path) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+#Read the header row first
+    csv_header = next(csvfile)
+
+#Read through each row of data after the header
+    for row in csvreader:
 
 
