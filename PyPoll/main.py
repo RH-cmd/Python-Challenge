60ed745f5fb1851edbb26507e5e86b1d4845cb58
Ronjini Hassan <ronjinihassan@ronjinis-mbp.home>
2020-12-02 21:59:54 -0500

    Calculayed total votes and list of candidates

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,9 +3,38 @@ import os
 import csv
 
 #Set path for file
-budget_path = os.path.join("Resources", "election_data.csv")
+election_data = os.path.join("Resources", "election_data.csv")
 
-#Create lists to hold data for candidates, number of votes for each candidate, and total percentage of votes for each candidate
+#Create lists to hold data for candidate names, number of votes for each candidate, and total percentage of votes for each candidate
 candidate_names = []
 number_of_votes = []
 percentage_of_votes = []
+
+#Set counter for the total number of votes
+total_number_of_votes = 0
+
+#Open election_data as a csv file
+with open(election_data, newline = "") as csvfile
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+#Read the header row first
+    csv_header = next(csvfile)
+
+    #Loop through each row 
+    for row in csvreader:
+
+        #Add votes to the counter
+        total_number_of_votes += 1
+
+        #Complete list of canidates who recieved votes by adding names to our list as they occur as well their vote count
+        if row[2] not in candidate_names
+            candidate_names.append(row[2])
+            index = candidate_names.index(row[2])
+            number_of_votes.append(1)
+        
+        else:
+            index = candidate_names.index(row[2])
+            number_of_votes[index] += 1
+
+        #Calculate the percentage of votes each candidate won
+    
